ecb37ec001307e4f759cf1af1281a44133539b8d
grandolf <internet@communautic.com>
2014-09-25 10:03:17 +0200

    Optimised TinyMCE setting

--- js/interface.js
+++ js/interface.js
@@ -1836,6 +1836,29 @@ $("select.saveSelect").on('change',function() {
 			{title: 'Neues Fenster', value: '_blank'},
 			{title: 'Keines', value: ''},
     	],
+		style_formats: [
+			{title: "Inline", items: [
+				{title: "Bold", icon: "bold", format: "bold"},
+				{title: "Italic", icon: "italic", format: "italic"},
+				{title: "Underline", icon: "underline", format: "underline"},
+				{title: "Strikethrough", icon: "strikethrough", format: "strikethrough"},
+				{title: "Superscript", icon: "superscript", format: "superscript"},
+				{title: "Subscript", icon: "subscript", format: "subscript"},
+				{title: "Code", icon: "code", format: "code"}
+			]},
+			{title: "Blocks", items: [
+				{title: "Paragraph", format: "p"},
+				{title: "Blockquote", format: "blockquote"},
+				{title: "Div", format: "div"},
+				{title: "Pre", format: "pre"}
+			]},
+			{title: "Alignment", items: [
+				{title: "Left", icon: "alignleft", format: "alignleft"},
+				{title: "Center", icon: "aligncenter", format: "aligncenter"},
+				{title: "Right", icon: "alignright", format: "alignright"},
+				{title: "Justify", icon: "alignjustify", format: "alignjustify"}
+			]}
+		],
 		paste_as_text: true,
 		toolbar: "undo redo | styleselect | bold italic | bullist numlist | link",
 		setup: tinymceSetup
